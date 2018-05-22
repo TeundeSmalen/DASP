6f47855484474a05c7ad8b66078e78ea2176ad85
Erik Hagenaars <erik-hagenaars@hotmail.com>
2018-05-22 14:37:26 +0200

    First function and folder

---
+++ functions/power_spectral_substraction.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = power_spectral_substraction(inputArg1,inputArg2)
+%POWER_SPECTRUM_SUBSTRACTION Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
